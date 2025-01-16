601b4bb30463b5b6b11b7935b55acfd54d597be4
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-01-16 16:52:28 +0500

    fixed

--- app_lightbulb/models.py
+++ app_lightbulb/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class  Type_of_premises(models.Model):
     title  =  models.CharField(max_length=300)
-    lk = models.PositiveIntegerField()
+    lk = models.PositiveIntegerField(null=True, blank=True)
 
 
     def __str__(self):
