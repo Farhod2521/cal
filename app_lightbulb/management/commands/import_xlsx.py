e2a8711a28cc42a4c48abe3dbd025bd5320d91d1
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-02-14 11:58:51 +0500

    fixed

--- app_lightbulb/management/commands/import_xlsx.py
+++ app_lightbulb/management/commands/import_xlsx.py
@@ -1,12 +1,12 @@
 import pandas as pd
 from django.core.management.base import BaseCommand
-from app_lightbulb.models import Room_Type_Category, Room_Type  # Modelingizning aniq joylashuvini tekshiring
+from app_lightbulb.models import Room_Type_Category, Room_Type
 
 class Command(BaseCommand):
     help = "Import data from xlsx file"
 
     def handle(self, *args, **kwargs):
-        file_path = "/home/user/backend/cal/app_lightbulb/management/commands/x.xlsx"  # Faylning to'liq joylashuvi
+        file_path = "/home/user/backend/cal/app_lightbulb/management/commands/x.xlsx"
 
         df = pd.read_excel(file_path, engine="openpyxl")
         df.columns = df.columns.str.strip()  # Ustun nomlaridan bo‘sh joylarni olib tashlash
@@ -29,13 +29,13 @@ class Command(BaseCommand):
             if not room_name:
                 continue
 
-            # Bo'sh qiymatlar uchun default qiymat
-            lk = int(row["lk"]) if pd.notna(row["lk"]) else 0
-            ra = int(row["ra"]) if pd.notna(row["ra"]) else 0
-            k = int(row["k"]) if pd.notna(row["k"]) else 0
-            table_height = float(row["table_height"]) if pd.notna(row["table_height"]) else 0
-            color_tem = row["color_tem"] if pd.notna(row["color_tem"]) else ""
-            light_type = row["light_type"] if pd.notna(row["light_type"]) else ""
+            # Bo'sh yoki "-" bo'lgan qiymatlar uchun default qiymat
+            lk = int(row["lk"]) if pd.notna(row["lk"]) and str(row["lk"]).strip() != "-" else 0
+            ra = int(row["ra"]) if pd.notna(row["ra"]) and str(row["ra"]).strip() != "-" else 0
+            k = int(row["k"]) if pd.notna(row["k"]) and str(row["k"]).strip() != "-" else 0
+            table_height = float(row["table_height"]) if pd.notna(row["table_height"]) and str(row["table_height"]).strip() != "-" else 0
+            color_tem = row["color_tem"] if pd.notna(row["color_tem"]) and str(row["color_tem"]).strip() != "-" else ""
+            light_type = row["light_type"] if pd.notna(row["light_type"]) and str(row["light_type"]).strip() != "-" else ""
 
             # Ma'lumotlarni saqlash
             Room_Type.objects.create(
