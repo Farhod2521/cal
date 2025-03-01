85029a3c8071c120a49abd853799b186f1f4d04c
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-03-01 13:24:34 +0500

    fixed

--- app_lightbulb/admin.py
+++ app_lightbulb/admin.py
@@ -2,10 +2,9 @@ from django.contrib import admin
 from .models import Type_of_premises, Room_Type, Room_Type_Category
 from mptt.admin import MPTTModelAdmin
 
-
-@admin.register(Room_Type_Category, MPTTModelAdmin)
-class Room_Type_CategoryAdmin(admin.ModelAdmin):
-    list_display = ['id','name']
+@admin.register(Room_Type_Category)
+class Room_Type_CategoryAdmin(MPTTModelAdmin, admin.ModelAdmin):
+    list_display = ['id', 'name']
 
 @admin.register(Room_Type)
 class RoomTypeAdmin(admin.ModelAdmin):
