35106cc59824c594382a9cc3cd5c3581c8ad8661
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-03-01 13:20:26 +0500

    fixed

--- app_lightbulb/admin.py
+++ app_lightbulb/admin.py
@@ -3,7 +3,9 @@ from .models import Type_of_premises, Room_Type, Room_Type_Category
 from mptt.admin import MPTTModelAdmin
 
 
-admin.site.register(Room_Type_Category, MPTTModelAdmin)
+admin.register(Room_Type_Category, MPTTModelAdmin)
+class Room_Type_CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id','name']
 
 @admin.register(Room_Type)
 class RoomTypeAdmin(admin.ModelAdmin):
