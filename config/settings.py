b9050a308a251a575616390daf2ea3c2a74b77b2
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-10-05 19:43:29 +0500

    fixed

--- config/settings.py
+++ config/settings.py
@@ -63,8 +63,26 @@ MIDDLEWARE = [
 ]
 ROOT_URLCONF = 'config.urls'
 CORS_ALLOW_ALL_ORIGINS = True
-
-
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
 CSRF_TRUSTED_ORIGINS = [
     "https://light-calc-rust.vercel.app",
     "https://cal.mkinfo.uz",
