4285ebafe480b719ba20ccbbe3e597cfe1406073
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-10-08 17:21:37 +0500

    fied

--- app_heating_calc/models.py
+++ app_heating_calc/models.py
@@ -11,8 +11,8 @@ class  Region(models.Model):
 class District(models.Model):
     region =  models.ForeignKey(Region, on_delete=models.CASCADE)
     name  =  models.CharField(max_length=200)
-    average_temperature = models.IntegerField()
-    time =  models.IntegerField()
+    average_temperature = models.FloatField()
+    time =  models.FloatField()
 
     def __str__(self):
         return self.name
